ed6813d192be18bd38b12fd2b69505d61bc6c754
salahlala <salahlala303@gmail.com>
2025-08-11 14:40:55 +0300

    refactor(Button) : change size of button

--- src/components/header/mode-toggle.tsx
+++ src/components/header/mode-toggle.tsx
@@ -20,7 +20,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu modal>
       <DropdownMenuTrigger asChild className="cursor-pointer">
-        <Button variant="outline" size="icon">
+        <Button variant="outline">
           <LuSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <LuMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         </Button>
