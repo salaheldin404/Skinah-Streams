50b51f73fe7c9daccefc4356ce5b10bd806b1156
salahlala <salahlala303@gmail.com>
2025-08-11 14:34:57 +0300

    style(FontControl) : adjust padding

--- src/components/font/FontControl.tsx
+++ src/components/font/FontControl.tsx
@@ -9,7 +9,7 @@ const FontControl = () => {
   const t = useTranslations("Settings");
 
   return (
-    <div className="space-y-5">
+    <div className="space-y-5 px-4">
       <h2 className="text-xl font-bold">{t("quran-font")}</h2>
       <div className="flex items-start md:items-center justify-between  gap-4 flex-col md:flex-row">
         <p>{t("style")}</p>
