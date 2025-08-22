68076d5faed302c3ba5d121f32c796c2211ab125
salahlala <salahlala303@gmail.com>
2025-08-22 03:02:01 +0300

    refactor(surah): Improve "last read" logic in TranslationContent

--- src/components/surah/TranslationContent.tsx
+++ src/components/surah/TranslationContent.tsx
@@ -4,10 +4,16 @@ import { Verse } from "@/types/verse";
 import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
 import { setOpenAudioPlayer } from "@/lib/store/slices/audio-slice";
 
-import { setSurahInfo, setClickedVerse } from "@/lib/store/slices/surah-slice";
+import {
+  setSurahInfo,
+  setClickedVerse,
+  setCurrentVerseLocation,
+  setLastRead,
+  setSaveMarkRead,
+} from "@/lib/store/slices/surah-slice";
 
 import TranslationDisplay from "./TranslationDisplay";
-import { useCallback, useEffect, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useRef } from "react";
 import LazyRender from "../verse/LazyRender";
 import { Surah } from "@/types/surah";
 interface TranslationContentProps {
@@ -24,7 +30,10 @@ const TranslationContent = ({ verses, surah }: TranslationContentProps) => {
     isOpen: isAudioPlayerOpen,
   } = useAppSelector((state) => state.audio);
 
-  const { lastRead } = useAppSelector((state) => state.surah);
+  const { lastRead, currentVerseLocation, saveMarkRead } = useAppSelector(
+    (state) => state.surah
+  );
+  const isSaveInProgress = useRef(false);
 
   const handleClickVerse = useCallback(
     (verse: Verse) => {
@@ -42,6 +51,37 @@ const TranslationContent = ({ verses, surah }: TranslationContentProps) => {
   }, []);
 
   const isReciterDisabled = useMemo(() => reciter.id === 0, [reciter.id]);
+
+  const handleVerseView = useCallback(
+    (data: {
+      hizb_number: number;
+      juz_number: number;
+      page_number: number;
+    }) => {
+      if (currentVerseLocation.page_number === data.page_number) return;
+      dispatch(setCurrentVerseLocation(data));
+    },
+    [dispatch, currentVerseLocation.page_number]
+  );
+  const handleAttemptSave = useCallback(
+    (verseData: Verse) => {
+      if (saveMarkRead && !isSaveInProgress.current) {
+        isSaveInProgress.current = true;
+
+        const lastReadData = {
+          chapter_id: verseData.chapter_id,
+          verse_number: verseData.verse_number,
+          page_number: verseData.page_number,
+          qpc_uthmani_hafs: verseData.qpc_uthmani_hafs,
+          verse_key: verseData.verse_key,
+        };
+
+        dispatch(setLastRead(lastReadData));
+        dispatch(setSaveMarkRead(false));
+      }
+    },
+    [saveMarkRead, dispatch]
+  );
   useEffect(() => {
     const targetId = lastRead?.verse_key; // Example: "1:31"
 
@@ -74,6 +114,8 @@ const TranslationContent = ({ verses, surah }: TranslationContentProps) => {
             verse={verse}
             key={verse.verse_key}
             isTarget={isTarget}
+            onVerseView={handleVerseView}
+            onAttemptSave={handleAttemptSave}
           >
             <TranslationDisplay
               verse={verse}
